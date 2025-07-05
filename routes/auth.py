9df6d73d769ce70a67e4714b7d63e01827341080
brunosandri <brunomsandri@gmail.com>
2025-07-05 12:42:23 -0400

    adiciona usuario e senha

--- routes/auth.py
+++ routes/auth.py
@@ -1,18 +1,10 @@
-from flask import Blueprint, request, jsonify
+import json
 from werkzeug.security import generate_password_hash, check_password_hash
 
-auth_bp = Blueprint('auth', __name__)
+with open("usuarios.json", "r", encoding="utf-8") as f:
+    raw_data = json.load(f)
 
-fake_users = {
-    "user@example.com": generate_password_hash("senha123")
+usuarios = {
+    email: generate_password_hash(dados["senha"])
+    for email, dados in raw_data.items()
 }
-
-@auth_bp.route('/login', methods=['POST'])
-def login():
-    data = request.json
-    email = data.get("email")
-    password = data.get("password")
-
-    if email in fake_users and check_password_hash(fake_users[email], password):
-        return jsonify({"token": "fake-token-for-" + email})
-    return jsonify({"error": "Login inválido"}), 401
