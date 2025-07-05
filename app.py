716defc91240c4887ceda8577f16217e672cd324
brunosandri <brunomsandri@gmail.com>
2025-07-05 10:31:14 -0400

    corrije app

--- app.py
+++ app.py
@@ -41,4 +41,6 @@ def upload_doc(email):
     return "Enviado com sucesso", 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
